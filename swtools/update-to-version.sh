e0c016920d696b6fb1775f645052d84cf97945e1
Goran Grbic <g.grbic@signundsinn.de>
2021-12-21 09:18:35 +0100

    Add link for update 5.7.6 version

--- swtools/update-to-version.sh
+++ swtools/update-to-version.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-curl -L  https://raw.githubusercontent.com/dnhsoft/shopware-tools/master/update-shop.sh > ./update-shop.sh
+curl -L  https://raw.githubusercontent.com/Tpojka/shopware-tools/master/update-shop.sh > ./update-shop.sh
 chmod +x ./update-shop.sh
 ./update-shop.sh $1
 rm ./update-shop.sh
